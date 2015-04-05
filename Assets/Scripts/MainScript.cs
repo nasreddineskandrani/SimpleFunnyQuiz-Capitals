ad9d654fe86d84bf07af5d027cc416de0c41d3e1
naskaa <nasreddine.skandrani@gmail.com>
2015-04-05 11:03:34 -0400

    Fix bug with generating bad answer (infinit loop case) - Add loop to questions when we hit the last one it restart from the beginning with a new order of questions

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -81,23 +81,23 @@ public class MainScript : MonoBehaviour {
 			if ( m_bCorrectAnswer ) {
 				++m_nScore;
 				++m_nQuestionsCounter;
+				++m_nCurrentId;
 			} else {
 				if (m_nScore > m_nSavedBestScore) { // if new best score
 					m_nSavedBestScore = m_nScore;
 					SaveBestScore();
 					m_textBestScore.text = "Best Score : " + m_nSavedBestScore;
 				}
-				m_nScore = 0;
-				m_nQuestionsCounter = 1;
+
+				ResetGame();
 			}
-			m_idCurrentQuestion.text = m_nQuestionsCounter + " / " + m_lQuestions.Count;
-			
-			++m_nCurrentId;
-			if (m_nCurrentId == m_nIds.Count)
+
+			if ( m_nQuestionsCounter > m_lQuestions.Count )
 			{
-				Shuffle(m_nIds);
-				m_nCurrentId = 0;
+				ResetGame();
 			}
+
+			m_idCurrentQuestion.text = m_nQuestionsCounter + " / " + m_lQuestions.Count;
 
 			for (int i = 0; i < 4; ++i)
 			{
@@ -105,13 +105,14 @@ public class MainScript : MonoBehaviour {
 				buttonImage = m_Btns[i].GetComponent<UnityEngine.UI.Image>();
 				buttonImage.overrideSprite = buttonNeutral;
 			}
+
 			NextQuestion();
+
 		}
 	}
 	
 	public void OnClickButton (Button p_clickedBtn) {
 		UnityEngine.UI.Image buttonImage;
-
 		if (!m_bAnimationEffectAnswer) {
 			//if good answer
 			if (p_clickedBtn.GetComponentInChildren<Text> ().text.CompareTo (m_lQuestions [m_nIds [m_nCurrentId]] ["response"]) == 0) { 
@@ -123,8 +124,8 @@ public class MainScript : MonoBehaviour {
 				buttonImage.overrideSprite = buttonBad;
 				m_bCorrectAnswer = false;
 			}
-
 			m_fRefTime = Time.time;
+
 			m_bAnimationEffectAnswer = true;
 		}
 	}
@@ -145,7 +146,7 @@ public class MainScript : MonoBehaviour {
 	}
 
 	public void NextQuestion() {
-
+		Debug.Log ("NextQuestion");
 		m_Text.text = m_lQuestions[m_nIds[m_nCurrentId]]["value"];
 
 		//emplacement question
@@ -170,8 +171,8 @@ public class MainScript : MonoBehaviour {
 				while (IsBannedId(nIdBadAnswer)) 
 				{
 					nIdBadAnswer = rd1.Next(0, m_nIds.Count);
-					m_nBannedIds.Add(nIdBadAnswer);
 				}
+				m_nBannedIds.Add(nIdBadAnswer);
 
 				badAnswer = m_lQuestions [m_nIds [nIdBadAnswer]] ["response"];
 				m_Btns[i].GetComponentInChildren<Text>().text = badAnswer;
@@ -191,17 +192,31 @@ public class MainScript : MonoBehaviour {
 		}
 		return false;
 	}
-
+	
 	public void Shuffle(List<int> list) {
-		int n = list.Count;
+		int counter = list.Count;
 		System.Random rnd = new System.Random();
-		while (n > 1) {
-			int k = (rnd.Next(0, n) % n);
-			n--;
+		while (counter > 1) {
+			int k = (rnd.Next(0, counter) % counter);
+			counter--;
 			int value = list[k];
-			list[k] = list[n];
-			list[n] = value;
+			list[k] = list[counter];
+			list[counter] = value;
 		}
+	}
+
+	public void ResetGame()
+	{
+		m_nCurrentId = 0;
+		m_nQuestionsCounter = 1;
+		
+		m_nIds.Clear();
+		
+		for (int i = 0; i < m_lQuestions.Count; ++i) {
+			m_nIds.Add(i);
+		}
+		
+		Shuffle(m_nIds);
 	}
 
 	public void SaveBestScore() {
